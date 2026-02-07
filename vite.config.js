a8436aa8e8b853db16b00619c2daaf296521b733
jakubminka <jakubminka@gmail.com>
2026-02-07 22:01:07 +0100

    fix: change base URL to / for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Minka-Weddings/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'docs',
